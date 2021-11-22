14293c207aba334969700235b47ca2a8d521a791
ishwork <ishworkhadka70@gmail.com>
2021-11-22 09:05:11 +0200

    initial commit

---
+++ src/components/CardDetail.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {useParams, useHistory} from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import axios from "axios";
+
+
+function CardDetail(){
+    const styles = useStyles();
+    const { id } = useParams();
+    const { push } = useHistory();
+    const [cardDetails, setCardDetails] = useState(null);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+                setCardDetails(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetch();
+    }, []);
+
+    if(!cardDetails) return null;
+
+    return(
+                <div className={styles.userCard}>
+                        <div>
+                            <Typography className={styles.userTypo}>- Name: {cardDetails.name}</Typography>
+                            <Typography className={styles.userTypo}>- Username: {cardDetails.username}</Typography>
+                            <Typography className={styles.userTypo}>- Email: {cardDetails.email}</Typography>
+                            <Typography className={styles.userTypo}>- Phone: {cardDetails.phone}</Typography>
+                            <Typography className={styles.userTypo}>- Website: {cardDetails.website}</Typography>
+                        </div>
+
+                        <div>
+                            <Typography className={styles.userTypo}>- Address:</Typography>
+                            <ul>
+                                <li><Typography className={styles.userTypo}>Street: {cardDetails.address.street}</Typography></li>
+                                <li><Typography className={styles.userTypo}>Suite: {cardDetails.address.suite}</Typography></li>
+                                <li><Typography className={styles.userTypo}>City: {cardDetails.address.city}</Typography></li>
+                                <li><Typography className={styles.userTypo}>Zipcode: {cardDetails.address.zipcode}</Typography></li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <Typography className={styles.userTypo}>- Geo:</Typography>
+                            <ul>
+                                <li><Typography className={styles.userTypo}>Latitude: {cardDetails.address.geo.lat}</Typography></li>
+                                <li><Typography className={styles.userTypo}>Longitude: {cardDetails.address.geo.lng}</Typography></li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <Typography className={styles.userTypo}>- Company:</Typography>
+                            <ul>
+                                <li><Typography className={styles.userTypo}>Name: {cardDetails.company.name}</Typography></li>
+                                <li><Typography className={styles.userTypo}>Catchphrase: {cardDetails.company.catchPhrase}</Typography></li>
+                                <li><Typography className={styles.userTypo}>BS: {cardDetails.company.bs}</Typography></li>
+                            </ul>
+                        </div>
+
+                        <br />
+                        <Button onClick={() => push('/')} variant="contained" color="primary">GO BACK</Button>
+                </div>
+    )
+}
+
+export default CardDetail;
+
+const useStyles = makeStyles({
+    userCard: {
+        position: 'fixed',
+        width: '50%',
+        height: "auto",
+        marginLeft: '40%',
+        marginTop: '50px'
+    },
+    userTypo: {
+        fontSize: '18px',
+        fontWeight: 'bold'
+    }
+});
---
+++ src/components/Home.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {useState, useEffect} from "react";
+import {Link} from 'react-router-dom';
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import axios from "axios";
+
+function Home() {
+    const styles = useStyles();
+    const[cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+                setCardData(data);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetch();
+    }, []);
+
+    return(
+     <div>
+         <Paper elevation={3} >
+             <Grid container spacing={2} style={{textAlign:'center'}}>
+                 {cardData.map((userData) => (
+                         <Grid item key={userData.id} XS={12} md={6} lg={4} style={{marginTop:'20px'}}>
+                             <Card className={styles.root}>
+                                 <CardContent>
+                                     <Avatar className={styles.imgAvatar}>{userData.name.charAt(0)}</Avatar>
+                                     <Typography className={styles.typoTitle} color="textSecondary" gutterBottom>
+                                         {userData.name}
+                                     </Typography>
+                                     <Typography className={styles.typoUsername} color="textSecondary" gutterBottom>
+                                         {userData.username}
+                                     </Typography>
+                                     <Typography className={styles.typoEmail} color="textSecondary" gutterBottom>
+                                         {userData.email}
+                                     </Typography>
+                                 </CardContent>
+
+                                 <Button component={Link} to={`/user/${userData.id}`} variant="contained" color="primary">
+                                     MORE DETAILS
+                                 </Button>
+                             </Card>
+                         </Grid>
+                     )
+                 )}
+             </Grid>
+         </Paper>
+     </div>
+    )
+}
+
+export default Home;
+
+const useStyles = makeStyles((theme) =>({
+    root: {
+        width: 300,
+        height: 300,
+        marginLeft: 100,
+        justifyContent: 'center',
+    },
+    imgAvatar: {
+      marginLeft: '40%',
+      marginBottom: '10px',
+      backgroundColor: 'orange',
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+    },
+    typoTitle: {
+        fontSize:18,
+        fontWeight: 'bold'
+    },
+    typoUsername: {
+        fontSize: 14,
+    },
+    typoEmail: {
+        fontSize: 14,
+        color: 'lightcoral'
+    },
+    }));
+
