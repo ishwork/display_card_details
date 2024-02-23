ea22980977a582e89f05233c9011092559ed19c1
ishwork <ishworkhadka70@gmail.com>
2024-02-23 15:27:07 +0200

    feat: add ga4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 import Home from './components/Home'
 import CardDetail from "./components/CardDetail";
 
 const App = () => {
+    ReactGA.initialize('G-C91FBSBC28');
+    useEffect(() => {
+        ReactGA.send({hitType: 'pageview', page: window.location.pathname + window.location.search, title: 'Home'});
+    }, []);
 
     return(
         <Router>
